97691f37b623f18e5fdaea232ccf61aad60bb91b
Yihengy37 <yihengy30@lakesideschool.org>
2025-04-29 17:47:57 -0700

    Update login.js

--- Code/login.js
+++ Code/login.js
@@ -51,7 +51,7 @@
     }
 
     fetch(
-      "https://raw.githubusercontent.com/TheHumblePotato/Yap-Window/refs/heads/main/Code/gui.js?token=$(date +%s)",
+      "https://raw.githubusercontent.com/Yihengy37/Yap-Window-Grammar/refs/heads/main/Code/gui.js?token=$(date +%s)",
     )
       .then((r) => r.text())
       .then((code) => {
@@ -73,7 +73,7 @@
             child,
           };
           fetch(
-            "https://raw.githubusercontent.com/TheHumblePotato/Yap-Window/refs/heads/main/Code/chat.js?token=$(date +%s)",
+            "https://raw.githubusercontent.com/Yihengy37/Yap-Window-Grammar/refs/heads/main/Code/chat.js?token=$(date +%s)",
           )
             .then((r) => r.text())
             .then((chatCode) => {
